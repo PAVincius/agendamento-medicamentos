2456219ee076c35978865fea4e9180d765a88388
PAVincius <vinicius.pratesaraujo@gmail.com>
2024-11-20 23:35:23 -0300

    feat(style): adding style to login page

--- app/submit-button.tsx
+++ app/submit-button.tsx
@@ -9,7 +9,7 @@ export function SubmitButton({ children }: { children: React.ReactNode }) {
     <button
       type={pending ? 'button' : 'submit'}
       aria-disabled={pending}
-      className="flex h-10 w-full items-center justify-center rounded-md border text-sm transition-all focus:outline-none"
+      className="flex h-10 w-full items-center justify-center rounded-md border text-sm transition-all focus:outline-none hover:bg-yellow-500 hover:text-white"
     >
       {children}
       {pending && (
